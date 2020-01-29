3773de8154ea545135cf1320151b366c0b46829d
patri <patri@206.21.94.210>
2020-01-29 11:05:24 -0500

    Login Attenot

--- JimStuff/src/Signup.java
+++ JimStuff/src/Signup.java
@@ -1,23 +1,31 @@
-import java.io.IOException;
+import java.io.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
- 
-@WebServlet(urlPatterns={"/login"})
+import javax.servlet.http.*;
+
+@WebServlet(urlPatterns={"/signup"})
 public class Signup extends HttpServlet
 {
+    static public User_Class users = User_Class.getInstance();
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
         resp.setContentType("text/plain");
-        var us = resp.getWriter();
-        var user = req.getParameter("username");
-        if( user == null ){
-            us.printf("No username provided");
-        } else {
-            String sess[];
-            var sess = req.getSession();
-            sess.setAttribute("name", user );
-            us.printf("Logged in as "+user);
+        var pw = resp.getWriter();
+        var name = req.getParameter("username");
+        var pass = req.getParameter("pass");
+        var real = req.getParameter("name");
+        if( name == null || pass == null || real == null)
+        {
+            pw.printf("Something is missing. Type this in format of the follwing" + "<br>");
+            pw.printf(".../signup?username=Your username&pass=Your password&name=Your Real Name");
+        }
+        else {
+            users.setusername(name);
+            pw.printf("Created username is "+name);
+            
+            users.setpasswords(pass);
+            pw.printf("\n" + "Password is " +pass);
+           
+            users.setname(real);
+            pw.printf("\n" + "Your name is " +real);
         }
     }
 
